d33867a267255cdffa4a0cdd587cfa8fe7376f80
cameronlattz <cameronlattz@gmail.com>
2019-04-16 17:11:25 -0500

    next

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,11 +15,10 @@ class Quiz extends Component {
         return(
             <div>
                 {isQuizEnd
-                        ? <QuizEnd />
+                        ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
                         : <QuizQuestion 
                             quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}
                             showNextQuestionHandler={this.showNextQuestion.bind(this)}
-                            resetClickHandler={this.handleResetClick.bind(this)}
                             />}
             </div>
         )
